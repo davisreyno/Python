c95b4259d5b44348c115a4a6936ebe3f8847ffb7
Reyno Davis <davisreynocs@gmail.com>
2024-08-30 15:00:53 +0000

    paddle OOP

--- Pong_Game/main.py
+++ Pong_Game/main.py
@@ -9,6 +9,7 @@
 # Keep score 
 
 from turtle import Screen, Turtle  
+from paddle import Paddle 
 
 # Create a screen 
 screen = Screen()
@@ -17,25 +18,16 @@ screen.bgcolor("black")
 screen.title("PONG")
 screen.tracer(0) # Turns of animation but will need refresh with screen.update() 
 
-# Create a paddle 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
+# Create left and right paddle 
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle(-350, 0)
 
-def go_up():
-    new_y = paddle.ycor() + 20 
-    paddle.goto(paddle.xcor(), new_y)
-
-def go_down():
-    new_y = paddle.ycor() - 20 
-    paddle.goto(paddle.xcor(), new_y)
-
+# Move paddles 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True 
 while game_is_on: 
