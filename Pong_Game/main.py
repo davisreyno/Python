0bf0ce38cd013468bdc3d0ac6641c82b68012839
Reyno Davis <davisreynocs@gmail.com>
2024-08-30 18:16:00 +0000

    Ball bounce logic

--- Pong_Game/main.py
+++ Pong_Game/main.py
@@ -34,4 +34,8 @@ while game_is_on:
     screen.update()
     ball.move()
 
+    # Detect ball collision with wall, Bounce
+    if ball.ycor() > 280 or ball.ycor() < -280
+        ball.bounce()
+
 screen.exitonclick()
