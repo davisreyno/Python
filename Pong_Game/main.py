141991e473a7666617ea1aca3bcb9ed48054057f
Reyno Davis <davisreynocs@gmail.com>
2024-08-30 18:32:34 +0000

    Detect ball out of bounds

--- Pong_Game/main.py
+++ Pong_Game/main.py
@@ -42,5 +42,13 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor () > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
          
+    # Detect right paddle miss 
+    if ball.xcor() > 380:
+        ball.reset_position() 
+
+    # Detect left paddle miss 
+if ball.xcor() < -380:
+        ball.reset_position() 
+        ball.reset_position() 
 
 screen.exitonclick()
