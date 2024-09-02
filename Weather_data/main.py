b51f3d2e68b485368f6fabe896da5f530d470836
Reyno Davis <davisreynocs@gmail.com>
2024-09-02 12:41:38 +0000

    Working with CSV data

--- Weather_data/main.py
+++ Weather_data/main.py
@@ -1,23 +1,24 @@
-# with open("weather_data.csv") as weather_data_file:
-#     weather_data = weather_data_file.readlines()
-#     print(weather_data)
+# Working with CSV data 
+
+# Simple open and read CSV
+with open("weather_data.csv") as weather_data_file:
+    weather_data = weather_data_file.readlines()
+    print(weather_data)
 
 
 # Using CSV libary with "reader" mode
+import CSV 
 
-# import CSV 
-
-# with open("weather_data.csv") as data_file: 
-#     data = csv.reader(data_file)
-#     temperatures = []
-#     for row in data:
-#         if row[1] != "temp":
-#             temperatures.append(int(row[1]))
-#     print(temperatures)
+with open("weather_data.csv") as data_file: 
+    data = csv.reader(data_file)
+    temperatures = []
+    for row in data:
+        if row[1] != "temp":
+            temperatures.append(int(row[1]))
+    print(temperatures)
 
 
 # Pandas - Python data analysis library. More info on pandas.pydata.org 
-
 import pandas 
 
 data = pandas.read_csv("weather_data.csv")
