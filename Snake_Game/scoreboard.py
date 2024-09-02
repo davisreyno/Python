54796d7b868c8ab2193e3d1862d8021d6079bb3e
Reyno Davis <davisreynocs@gmail.com>
2024-09-02 11:01:33 +0000

    Keeping high score with data.txt

--- Snake_Game/scoreboard.py
+++ Snake_Game/scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0 
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
         self.color("black")
         self.penup()
         self.goto(0, 270)
@@ -19,13 +20,15 @@ class Scoreboard(Turtle):
         self.clear
         self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
+    # High score 
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}") 
         self.score = 0
         self.update_scoreboard
 
     def increase_score(self): 
         self.score += 1
         self.update_scoreboard()
-        
