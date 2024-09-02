1eefac619367941a8ddc9a71b252ddf0fb59c0d2
Reyno Davis <davisreynocs@gmail.com>
2024-09-02 10:44:51 +0000

    High score added

--- Snake_Game/snake.py
+++ Snake_Game/snake.py
@@ -28,6 +28,13 @@ class Snake:
         new_segment.goto(position)
         self.segments.append(new_segment)
 
+    def reset(self):
+        for seg in self.segments:
+            self.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     #Add a new segment to snake after every food 
     def extend(self): 
         self.add_segment(self.segments[-1].position())
