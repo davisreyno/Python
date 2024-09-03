01aea83a02cabc0ea4b2e03460d3104b25f4549b
davisreyno <davisreynocs@gmail.com>
2024-09-03 13:42:03 -0500

    Update to guiz

--- Name_the_States/main.py
+++ Name_the_States/main.py
@@ -14,11 +14,17 @@ guessed_states = []
 
 while len(guessed_states) < 50:
     # Pop up block for user input
-    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?")
-    print(answer_state)
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
 
-    # If answer state is one of the states in all the states of the 50 states
-    # If user is correct, create a turtle to write the name of the state at respective coor
+    if answer_state == "exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        # Creates CSV of missing states from quiz to learn
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
@@ -29,7 +35,3 @@ while len(guessed_states) < 50:
         t.write(answer_state)
 
 screen.exitclick()
-
-# # Keeps screen open
-# turtle.onscreenclick(get_mouse_click_coor)
-# turtle.mainloop()
