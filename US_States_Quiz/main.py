5e0a7525b593cd5d0aa3848ae2eeb77dd06f485b
Reyno Davis <davisreynocs@gmail.com>
2024-09-03 11:57:11 +0000

    Code

--- US_States_Quiz/main.py
+++ US_States_Quiz/main.py
@@ -1,8 +1,10 @@
 import turtle 
 
+# Creates screen with blank states image
 screen = turtle.Screen()
 screen.title("U.S. States Quiz")
 image = "blank_states_img.gif"
 screen.addshape(image)
+turtle.shape(image)
 
 screen.exitonclick()
